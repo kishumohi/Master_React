5abae3f22c6680860874f97303212e93e823e61d
kishan <kishankalavadiya@gmail.com>
2024-11-20 15:04:19 +0530

    04_MegaBlog_Redux Store Setup and Tailwind CSS Config Header and Footer

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+// import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>Blog App With Appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .GetCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
---
+++ MegaBlog/src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import AuthReducer from "./authSlice.js";
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
