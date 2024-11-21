14727e5dfdda290bbc9351908a1eecc7664a9af1
kishan <kishankalavadiya@gmail.com>
2024-11-21 11:06:41 +0530

    07_MegaBlog_Page Create as AddPost, Allpost, EditPost, Home, Login, Post, Signup

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -7,6 +7,10 @@ import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 import RTE from "./RTE.jsx";
 import Select from "./Select.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import PostCard from "./PostCard.jsx";
+import PostForm from "./post-form/PostForm.jsx";
 
 export {
   Select,
@@ -18,4 +22,8 @@ export {
   Footer,
   LogoutBtn,
   RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
 };
---
+++ MegaBlog/src/pages/AddPost.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Container, Post, PostForm } from "../components/index.js";
+
+function AddPost() {
+  return (
+    <div className="py-8">
+      <Container>
+        <PostForm />
+      </Container>
+    </div>
+  );
+}
+
+export default AddPost;
